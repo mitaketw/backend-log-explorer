78270e09dd2cc0e43d5bd2b5eefe01ad1fce4159
kewang <cpckewang@gmail.com>
2018-04-24 18:21:48 +0800

    Send log from POST method

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+app.post('/log', function(req, res){
+  console.log(req.body);
+
+  io.emit('chat message', req.body);
+
+  res.end();
 });
 
 http.listen(3000, function(){
