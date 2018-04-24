5c1e886aa1f32984113e1e5282ded228d34a802d
kewang <cpckewang@gmail.com>
2018-04-24 18:25:36 +0800

    Send log via text/plain

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
