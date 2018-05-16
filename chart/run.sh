cf875a9a48be5fbbfe6df0b5a833007bfd89cffd
kewang <cpckewang@gmail.com>
2018-05-16 15:22:53 +0800

    Update run script

--- chart/run.sh
+++ chart/run.sh
@@ -13,13 +13,26 @@ do
   eval $CMD
 done
 
-#echo $TITLE
-#echo $DESCRIPTION
-#echo $SQL
-#echo $INPUT
-#echo $OUTPUT
+TMP=$(echo $INPUT | jq -r -c '.[] | to_entries')
+VAR_INPUT_KEY=($(echo $TMP | jq -r -c '.[].key'))
+VAR_INPUT_VALUE=($(echo $TMP | jq -r -c '.[].value'))
 
-#impala-shell --print_header -B -o /dev/stdout --quiet -f /dev/stdin | 
-#csvtojson --delimiter='\t' |
-#./jsontohighcharts |
-#highcharts-export-server --infile /dev/stdin --outfile test.png
+for k in "${!VAR_INPUT_KEY[@]}"
+do
+  CMD='VALUE='''${VAR_INPUT_VALUE[$k]}''
+  
+  eval $CMD
+
+  SQL=$(echo $SQL | sed "s/:${VAR_INPUT_KEY[$k]}/$VALUE/g")
+done
+
+echo $TITLE
+echo $DESCRIPTION
+echo $SQL
+echo $INPUT
+echo $OUTPUT
+
+impala-shell --print_header -B -o /dev/stdout --quiet -q $SQL | 
+csvtojson --delimiter='\t' |
+./jsontohighcharts |
+highcharts-export-server --infile /dev/stdin --outfile test.png
