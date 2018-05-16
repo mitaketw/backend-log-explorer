46deadcea97c4e107ca48482bc3ecac922bd94a9
kewang <cpckewang@gmail.com>
2018-05-16 18:52:18 +0800

    Add chart.pug to show charts

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var app = require('express')();
+var glob = require('glob');
+var path = require("path");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.text());
 
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/chart', function(req, res){
+  var files = glob.sync(__dirname + "/chart/*.yml");
+
+  var filenames = files.map(function(file){
+    return path.basename(file);
+  });
+
+  res.render("chart.pug", {
+    filenames: filenames
+  });
 });
 
 app.post('/log', function(req, res){
