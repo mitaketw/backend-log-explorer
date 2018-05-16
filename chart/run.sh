5dbd6ab64d42f14490e89f4bfe6edf41cfa31435
kewang <cpckewang@gmail.com>
2018-05-16 17:42:45 +0800

    Using filename as input file

--- chart/run.sh
+++ chart/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-YAML=$(js-yaml chart.yml)
+YAML=$(js-yaml $1)
 
 KEY=($(echo $YAML | jq -r -c 'to_entries | .[].key'))
 
@@ -13,17 +13,15 @@ do
   eval $CMD
 done
 
-TMP=$(echo $INPUT | jq -r -c '.[] | to_entries')
-VAR_INPUT_KEY=($(echo $TMP | jq -r -c '.[].key'))
-VAR_INPUT_VALUE=($(echo $TMP | jq -r -c '.[].value'))
+INPUT_KEY=($(echo $INPUT | jq -r -c '.[]'))
 
-for k in "${!VAR_INPUT_KEY[@]}"
+for k in "${!INPUT_KEY[@]}"
 do
-  CMD='VALUE='''${VAR_INPUT_VALUE[$k]}''
+  CMD='VALUE=$'''$((k+2))''
   
   eval $CMD
 
-  SQL=$(echo $SQL | sed "s/:${VAR_INPUT_KEY[$k]}/$VALUE/g")
+  SQL=$(echo $SQL | sed "s/:${INPUT_KEY[$k]}/$VALUE/g")
 done
 
 TMP=($(echo $OUTPUT | jq -r -c '.[] | .X + .Y'))
@@ -39,4 +37,4 @@ echo $OUTPUT
 impala-shell --print_header -B -o /dev/stdout --quiet -q "$SQL" | 
 csvtojson --delimiter='\t' |
 ./jsontohighcharts $XAXIS $YAXIS |
-highcharts-export-server --infile /dev/stdin --outfile test.png
+highcharts-export-server --infile /dev/stdin --outfile $TITLE.png
